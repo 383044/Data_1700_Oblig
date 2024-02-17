a7e99f11c40c29f0f6b1560cf003e81a41b273cc
Ivan <evan.akkash95@gmail.com>
2024-02-18 00:31:54 +0100

    Ferdig projekt, klar for innlevering.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Data_1700_Oblig1
 ===================
- OsloMet brukernavn: ivoth0002
+
+OsloMet brukernavn: ivoth0002
  
 GitHub brukernavn: 383044
 
@@ -9,3 +10,10 @@ GitHub repo URL: https://github.com/383044/Data_1700_Oblig1
 Fult navn: Ivan Othman
 
 Kort beskrivelse av applikasjon:
+Prosjektet er en nettapplikasjon for bestilling av kinobilletter. 
+På nettsiden kan brukere velge en film fra en rullegardinliste, 
+angi antall billetter de ønsker, og fylle ut personlig informasjon 
+som fornavn, etternavn, telefonnummer og e-postadresse.
+Når brukeren klikker på "Kjøp billett"-knappen, blir bestillingsinformasjonen 
+lagt til i en tabell under skjemaet. Det er også mulig å slette alle billettbestillinger
+ved å klikke på knappen "Slett alle billetter". 
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,55 @@
+// En funksjon som tar imot bestillinger og skriver dem ut.
+function bestill() {
+    //tabellen som skal inneholde utskriften av bestillingene
+     let billetter_liste = document.getElementById("billetter");
+
+    // henting av input values
+     const innFilm = document.getElementById("film").value;
+     const innAntall = document.getElementById("antall").value;
+     const innFornavn = document.getElementById("fornavn").value;
+     const innEtternavn = document.getElementById("etternavn").value;
+     const innTelefonnr = document.getElementById("telefonnr").value;
+     const innEpost = document.getElementById("epost").value;
+
+
+     //Definering av et objekt for input-dataene.
+     let enBestilling = {
+         film: innFilm,
+         antall: innAntall,
+         fornavn: innFornavn,
+         etternavn: innEtternavn,
+         telefonnr: innTelefonnr,
+         epost: innEpost
+     };
+    // Opprettelse av et array og innlemming av objektet i det ved hjelp av push-metoden.
+     const bestillingArray = [];
+     bestillingArray.push(enBestilling);
+
+     //For-løkke som går gjennom hele arrayet og legger inneholdet til tabelen.
+     for (let i of bestillingArray) {
+         let ut = "<tr><td class='output'>" + i.film + "</td><td class='output'>"+ i.antall +"</td><td class='output'>"+ i.fornavn + "</td><td class='output'>" + i.etternavn +
+             "</td><td class='output'>" + i.telefonnr + "</td><td>" + i.epost + "</td></tr>";
+         billetter_liste.insertAdjacentHTML("beforeend", ut);
+
+         // en del av løkke, å blanke input-feltene.
+        document.getElementById("film").value = "";
+        document.getElementById("antall").value = "";
+        document.getElementById("fornavn").value = "";
+        document.getElementById("etternavn").value = "";
+        document.getElementById("telefonnr").value = "";
+        document.getElementById("epost").value = "";
+     }
+
+     //Hvis funksjonen blir kalt, returnerer den det oppdaterte arrayet. Det ble ikke brukt i koden, men det er kjekt å ha.
+    return bestillingArray;
+ }
+
+ // En annen funksjon som sletter alle output "utskriften".
+function slett(){
+    const output_tabell = document.getElementById("billetter");
+    const rader = output_tabell.getElementsByTagName("tr")
+     const antall_rader = rader.length;
+    for (let i= 1; i<antall_rader; i++){    //alltid rad med index nr1 blir slettet, for at tabellen oppdateres etter hver
+        output_tabell.deleteRow(1)              //rad-sletting og jeg vil alltig slette rad nr1, som ligger under table-head
+    }
+ }
